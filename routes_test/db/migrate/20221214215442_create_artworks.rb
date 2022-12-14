5ec0063c5897846ab1122b8d3d2ec7d253bb33e1
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2022-12-14 17:03:18 -0500

    started art project

---
+++ routes_test/db/migrate/20221214215442_create_artworks.rb
@@ -0,0 +1,11 @@
+class CreateArtworks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :artworks do |t|
+      t.string :title, null: false
+      t.string :img_url, null: false
+      t.references :artist, null: false, foreign_key: {to_table: :users}
+      t.timestamps
+    end
+    add_index :artworks, :img_url, unique: true
+  end
+end
