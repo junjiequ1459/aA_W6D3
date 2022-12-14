02d5a14cb4370a75ec1f95178f450b00931aa30f
rexqu <junjiequ123@gmail.com>
2022-12-14 15:25:28 -0500

    worked on controllers

--- routes_test/app/controllers/users_controller.rb
+++ routes_test/app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
-    
+  def index
+    render plain: "im in the index action"
+  end
+
+  def create
+    render json: params
+  end
+
+  def show
+    render json: params
+  end
 end
