9943fdd7a6d89b392f6ce154dc52b1bfa5a957d0
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2022-12-14 14:58:53 -0500

    did the user controller thing

--- routes_test/config/routes.rb
+++ routes_test/config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
-  # get "users", to: "users#index", as: "user"
-  # post "users", to: "users#create", as: "user"
-  # get "new_user", to: "users#new", as: "user"
-  # get "edit_user", to: "users#edit", as: "user"
-  # get "user/:id", to: "users#show", as: "user"
-  # patch "user", to: "users#update", as: "user"
-  # put "user", to: "users#update", as: "user"
-  # delete "user", to: "users#destroy", as: "user"
+  # resources :users
+  get "users", to: "users#index", as: "users"
+  post "users", to: "users#create"
+  get "user/new", to: "users#new", as: "new_user"
+  get "users/:id/edit", to: "users#edit", as: "edit_user"
+  get "user/:id", to: "users#show", as: "user"
+  patch "user/:id", to: "users#update"
+  put "user/:id", to: "users#update"
+  delete "user/:id", to: "users#destroy"
 end
