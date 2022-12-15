002896225abea9bd42c6d7d2f0c3d6f501ffff30
rexqu <junjiequ123@gmail.com>
2022-12-15 09:58:06 -0500

    update

--- routes_test/app/models/artwork.rb
+++ routes_test/app/models/artwork.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  img_url    :string           not null
+#  artist_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Artwork < ApplicationRecord
+  validates :title, uniqueness: { scope: :artist_id, message: "artist should not have multiple works of the same name" }
 end
--- routes_test/app/models/user.rb
+++ routes_test/app/models/user.rb
@@ -3,12 +3,10 @@
 # Table name: users
 #
 #  id         :bigint           not null, primary key
-#  name       :string           not null
-#  email      :string           not null
+#  username   :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true 
+  validates :username, presence: true
 end
