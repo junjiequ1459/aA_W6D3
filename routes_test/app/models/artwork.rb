d0e503656b236f9ac0204f814e41860f62467016
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2022-12-15 10:29:23 -0500

    added ArtworkShares

--- routes_test/app/models/artwork.rb
+++ routes_test/app/models/artwork.rb
@@ -11,4 +11,15 @@
 #
 class Artwork < ApplicationRecord
   validates :title, uniqueness: { scope: :artist_id, message: "artist should not have multiple works of the same name" }
+
+    belongs_to :artist,
+      primary_key: :id,
+      foreign_key: :artist_id,
+      class_name: :User
+
+    has_many :shares,
+      primary_key: :id,
+      foreign_key: :artwork_id,
+      class_name: :ArtworkShare
+
 end
